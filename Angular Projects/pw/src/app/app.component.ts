22ccc5556eccf91a4c9928ad304cf919d8c04406
Mkhululi-98 <mkululi98@gmail.com>
2023-04-19 13:52:09 +0200

    Password project

--- Angular Projects/pw/src/app/app.component.ts
+++ Angular Projects/pw/src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = "";
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  OnChangeLength(value: string){
+      const parseValue = parseInt(value);
+      if(!isNaN(parseValue)){
+          this.length = parseValue;
+      }
+  }
+
+  onChangeLetters(){
+    this.includeLetters = !this.includeLetters;
+    console.log(this.includeNumbers);
+  }
+
+  onChangeNumbers(){
+    this.includeNumbers = !this.includeLetters;
+  }
+  onChangeSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick(){
+    let nums = "1234567890";
+    let letters = "abcdefghijklmnopqrstuvwxyz";
+    let syms = "!@#$%^&*()_+=~`<>?';{}[]";
+
+    let temp = "";
+    if(this.includeLetters){
+      temp += letters;
+  
+    }
+    
+    if(this.includeNumbers){
+      temp += nums;
+    }
+    
+    if(this.includeSymbols){
+      temp += syms;
+    }
+    let pass = "";
+    for(let i = 0;i < this.length;i++){
+      const ind = Math.floor(Math.random()* temp.length);
+      pass += temp[ind]; 
+    }
+    this.password = pass;
+  }
 }
