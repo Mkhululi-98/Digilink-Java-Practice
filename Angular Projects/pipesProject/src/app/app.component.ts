22cde92c3e4593deb1c04ada6ba70749955620f6
Mkhululi-98 <mkululi98@gmail.com>
2023-04-21 10:52:32 +0200

    pipes

---
+++ Angular Projects/pipesProject/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = "";
+  date = "";
+  amount!: number;
+  height!: number;
+  miles!: number;
+
+  car = {
+    make: "Ford",
+    model: "Everest",
+    year: 2022
+  }
+
+  onNameChange(value: string){
+    this.name = value;
+  }
+  onDateChange(value: string){
+    this.date = value;
+  }
+  onAmountChange(value: string){
+    this.amount = parseFloat(value);
+  }
+  onHeightChange(value: string){
+    this.height = parseFloat(value);
+  }
+  onMilesChange(value: string){
+    this.miles = parseFloat(value);
+  }
+}
