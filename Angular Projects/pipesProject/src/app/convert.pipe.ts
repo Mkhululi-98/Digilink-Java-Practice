c9fdffa5dfca2558da8e301619661bed7dc0aff4
Mkhululi-98 <mkululi98@gmail.com>
2023-04-21 11:16:58 +0200

    chaining pipes addition

--- Angular Projects/pipesProject/src/app/convert.pipe.ts
+++ Angular Projects/pipesProject/src/app/convert.pipe.ts
@@ -5,9 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: any, ...args: unknown[]): unknown {
+  transform(value: any,unit: string): any {
     if(!value){
       return "";
+    }
+    switch(unit){
+      case "m":
+        return value * 1.609*1000;
+      case "km": 
+        return value * 1.609;
+      case "cm":
+        return value * 1.609*1000*1000;
+      default:
+        throw new Error("Unit not supported")
     }
     return value * 1.609;
   }
